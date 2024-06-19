ac043fc91ef23c06bf04bb7e1cdbcef1670efa7b
Lucas Nery <lucas.leite1@br.nestle.com>
2024-06-19 10:39:28 -0300

    fix build

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const peopleRoutes = require('./src/routes/peopleRoutes');
 const categorysRoutes = require('./src/routes/categorysRoutes');
 const billRoutes = require('./src/routes/billRoutes');
 const firebaseRoutes = require('./src/routes/firebaseRoutes');
-const verifyToken = require('./src/middleware');
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -24,5 +23,5 @@ app.use('/api', categorysRoutes);
 app.use('/api', billRoutes);
 
 app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
+  console.log(`Servidor rodando em http://render.com:${port}`);
 });
